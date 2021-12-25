a057b521ee3e14a0a6dca02c4abee7438f3a399b
Jagiro Elmodovar <jagiro@walla.com>
2021-12-25 14:57:29 +0200

    create parcel react app

---
+++ src/app/App.js
@@ -0,0 +1,5 @@
+export default function App() {
+    return (
+        <div>hi from app. start kliding bitchface.</div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDom from 'react-dom'
+import App from './app/App'
+
+const root = document.querySelector('#root')
+ReactDom.render(<App />, root)
